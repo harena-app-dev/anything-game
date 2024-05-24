bf0e1db473039c8440a930f23801a0bddb5d1e56
harena-app-dev <harena.app.dev@gmail.com>
2024-05-24 18:46:51 -0500

    c

--- scripts/server/main.ts
+++ scripts/server/main.ts
@@ -7,17 +7,6 @@ export const wsm = new WebSocketMessager();
 const entityRegistry = new Registry();
 export const serverEntityRegistry = new ServerRegistry(wsm);
 
-function registerHandlers() {
-	const handlerFileNames = fs.readdirSync(__dirname + '/handlers');
-	for (const handlerFileName of handlerFileNames) { 
-		const handlerName = handlerFileName.split('.')[0];
-		console.log(`Registering handler: ${handlerName}`);
-		const handler = require(`./handlers/${handlerFileName}`).default;
-		wsm.addHandler(handlerName, handler);
-	}
-	
-}
-// registerHandlers();
 wsm.addHandler('consoleMessages', (ws, _) => {
 	wsm.send(ws, 'consoleMessages', ['Hello from server', 'Hello from server 2']);
 });
