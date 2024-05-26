f3c537c705f0da343fd1116bb0326dc5025ce437
harena-app-dev <harena.app.dev@gmail.com>
2024-05-26 10:29:15 -0500

    r

--- components/EntityView.jsx
+++ components/EntityView.jsx
@@ -17,41 +17,7 @@ import { Stack } from "@mui/material";
 import { styled } from '@mui/material/styles';
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 
-const Accordion = styled((props) => (
-	<MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-	border: `1px solid ${theme.palette.divider}`,
-	'&:not(:last-child)': {
-		borderBottom: 0,
-	},
-	'&::before': {
-		display: 'none',
-	},
-}));
-
-const AccordionSummary = styled((props) => (
-	<MuiAccordionSummary
-		expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-		{...props}
-	/>
-))(({ theme }) => ({
-	backgroundColor:
-		theme.palette.mode === 'dark'
-			? 'rgba(255, 255, 255, .05)'
-			: 'rgba(0, 0, 0, .03)',
-	flexDirection: 'row-reverse',
-	'& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-		transform: 'rotate(90deg)',
-	},
-	'& .MuiAccordionSummary-content': {
-		marginLeft: theme.spacing(1),
-	},
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-	padding: theme.spacing(2),
-	borderTop: '1px solid rgba(0, 0, 0, .125)',
-}));
+import { Accordion, AccordionSummary, AccordionDetails } from './Accordion';
 
 export default function ({ entity, registry }) {
 	const [anchorEl, setAnchorEl] = React.useState(null);
