c4a68914b137c80867bffe5b0e041c713dcc1964
harena-app-dev <harena.app.dev@gmail.com>
2024-05-27 13:21:54 -0500

    c

--- scripts/server/main.js
+++ scripts/server/main.js
@@ -3,7 +3,6 @@ import WebSocketMessager from "./WebSocketMessager";
 import { NetworkedRegistry } from "../NetworkedRegistry";
 
 export const wsm = new WebSocketMessager();
-// export const serverEntityRegistry = new NetworkedRegistry();
 export const registry = NetworkedRegistry();
 registry.onCreate.connect(({ entity }) => {
 	wsm.sendToAll('newMessage', `Entity ${entity} created`);
