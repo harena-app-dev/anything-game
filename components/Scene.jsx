f36a1236ade5ec6110096c59e86fb6c2543a0580
harena-app-dev <harena.app.dev@gmail.com>
2024-05-26 19:12:19 -0500

    three

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,9 +1,33 @@
-import {Accordion, AccordionDetails, AccordionSummary} from './Accordion';
+import { Accordion, AccordionDetails, AccordionSummary } from './Accordion';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { Stack } from "@mui/material";
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
+function Box(props) {
+	// This reference gives us direct access to the THREE.Mesh object
+	const ref = useRef()
+	// Hold state for hovered and clicked events
+	const [hovered, hover] = useState(false)
+	const [clicked, click] = useState(false)
+	// Subscribe this component to the render-loop, rotate the mesh every frame
+	useFrame((state, delta) => (ref.current.rotation.x += delta))
+	// Return the view, these are regular Threejs elements expressed in JSX
+	return (
+		<mesh
+			{...props}
+			ref={ref}
+			scale={clicked ? 1.5 : 1}
+			onClick={(event) => click(!clicked)}
+			onPointerOver={(event) => hover(true)}
+			onPointerOut={(event) => hover(false)}>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+		</mesh>
+	)
+}
 export default function Scene({ registry }) {
 	const [anchorEl, setAnchorEl] = React.useState(null);
 	const open = Boolean(anchorEl);
@@ -13,6 +37,11 @@ export default function Scene({ registry }) {
 	const handleClose = () => {
 		setAnchorEl(null);
 	};
+	if (!navigator.gpu) {
+		throw new Error("WebGPU not supported on this browser.");
+	} else {
+		console.log("WebGPU supported!");
+	}
 	return (
 		<Accordion defaultExpanded className="col grow">
 			<AccordionSummary
@@ -23,13 +52,13 @@ export default function Scene({ registry }) {
 				Scene
 			</AccordionSummary>
 			<AccordionDetails className="col grow">
-				<Stack spacing={2}>
-				</Stack>
-				<TextField
-					label="Enter a command"
-					placeholder="Command"
-					multiline
-				/>
+				<Canvas className="">
+					<ambientLight intensity={Math.PI / 2} />
+					<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+					<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+					<Box position={[-1.2, 0, 0]} />
+					<Box position={[1.2, 0, 0]} />
+				</Canvas>
 			</AccordionDetails>
 		</Accordion>
 	);
