18cc2971a07cf90fc569ca27bfd56888e362e0f7
harena-app-dev <harena.app.dev@gmail.com>
2024-05-28 15:22:05 -0500

    mui

--- components/ecs/RegistryView.jsx
+++ components/ecs/RegistryView.jsx
@@ -149,34 +149,7 @@ function EnhancedTableToolbar(props) {
 		{ icon: <AddIcon />, name: 'Create', onClick: () => registry.cmdCreate() },
 	];
 	return (
-		<Toolbar
-			sx={{
-				...(numSelected > 0 && {
-					bgcolor: (theme) =>
-						alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-				}),
-			}}
-		>
-			{numSelected > 0 ? (
-				<Typography
-					sx={{ flex: '1 1 100%' }}
-					color="inherit"
-					variant="subtitle1"
-					component="div"
-				>
-					{numSelected} selected
-				</Typography>
-			) : (
-				<Typography
-					sx={{ flex: '1 1 100%' }}
-					variant="h6"
-					id="tableTitle"
-					component="div"
-				>
-					Registry
-				</Typography>
-			)}
-
+		<Stack direction="column" spacing={0}>
 			{numSelected > 0 ? (
 				<Tooltip title="Delete">
 					<IconButton onClick={() => {
@@ -198,7 +171,7 @@ function EnhancedTableToolbar(props) {
 
 				</BasicMenu>
 			)}
-		</Toolbar>
+		</Stack>
 	);
 }
 
