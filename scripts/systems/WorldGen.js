041537ac7053768d04ef85c639f95b9eca36d982
harena-app-dev <harena.app.dev@gmail.com>
2024-05-28 23:36:28 -0500

    trees

--- scripts/systems/WorldGen.js
+++ scripts/systems/WorldGen.js
@@ -7,6 +7,12 @@ export default function ({ registry }) {
 			const entity = registry.create()
 			registry.emplace({ type: "Position", entity, component: { x, y, z: 0 } })
 			registry.emplace({ type: "Sprite", entity, component: { path: "dirt.png" } })
+			const rng = Math.random()
+			if (rng < 0.1) {
+				const tree = registry.create()
+				registry.emplace({ type: "Position", entity: tree, component: { x, y, z: 0.01 } })
+				registry.emplace({ type: "Sprite", entity: tree, component: { path: "tree.png" } })
+			}
 		}
 	}
 	return system
