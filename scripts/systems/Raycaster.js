2304251aa475b196115def61b222b99134c6d2f4
harena-app-dev <harena.app.dev@gmail.com>
2024-05-29 15:01:23 -0500

    w

--- scripts/systems/Raycaster.js
+++ scripts/systems/Raycaster.js
@@ -9,11 +9,13 @@ export default function ({ registry, sceneElement, spriteRenderer, scene }) {
 			this.raycaster.setFromCamera(this.pointer, camera);
 		}
 	}
-	registry.onEmplace({type: 'Selection', callback: ({ entity }) => {
-		const sprite = registry.emplace({ entity, type: 'Sprite', component: { path: 'selectionSquare.png' } });
-		const selection = registry.get(entity, 'Selection');
-		const parent = registry.emplace({ entity, type: 'Parent', component: { entity: selection.entity } });
-	}});
+	registry.onEmplace({
+		type: 'Selection', callback: ({ entity }) => {
+			const sprite = registry.emplace({ entity, type: 'Sprite', component: { path: 'selectionSquare.png' } });
+			const selection = registry.get(entity, 'Selection');
+			const parent = registry.emplace({ entity, type: 'Parent', component: { entity: selection.entity } });
+		}
+	});
 	function onPointerMove(event) {
 		const offset = sceneElement.getBoundingClientRect();
 		system.pointer.x = ((event.clientX - offset.left) / sceneElement.clientWidth) * 2 - 1;
@@ -28,7 +30,12 @@ export default function ({ registry, sceneElement, spriteRenderer, scene }) {
 			const selectionEntity = registry.create();
 			registry.emplace({ type: 'Selection', entity: selectionEntity, component: { entity } });
 		} else {
-			registry.each({ types: ['Selection'], callback: ({ entity }) => registry.destroy(entity) });
+			registry.each({
+				types: ['Selection'], callback: ({ entity }) => {
+					console.log('destroying selection entity', entity);
+					registry.destroy({ entity });
+				}
+			});
 		}
 	}
 	window.addEventListener('pointerdown', onPointerDown);
