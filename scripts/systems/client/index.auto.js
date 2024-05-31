3c0492830ea0b183e5a096bd2c1eb1b5b20c7dad
harena-app-dev <harena.app.dev@gmail.com>
2024-05-31 15:23:57 -0500

    w

--- scripts/systems/client/index.auto.js
+++ scripts/systems/client/index.auto.js
@@ -2,5 +2,5 @@ import Client from './Client.js';
 export { Client };
 import KeyboardState from './KeyboardState.js';
 export { KeyboardState };
-import Spawner from './Spawner.js';
-export { Spawner };
+import Player from './Player.js';
+export { Player };
