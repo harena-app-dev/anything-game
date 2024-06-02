d453ef736c96471c33b965dbc1f571ddf6270057
harena-app-dev <harena.app.dev@gmail.com>
2024-06-02 10:55:21 -0500

    w

--- components/status/Status.jsx
+++ components/status/Status.jsx
@@ -2,6 +2,7 @@ import React, { use, useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import { Alert, LinearProgress, Stack, TextField, Typography, linearProgressClasses, styled } from '@mui/material';
 import Log from '@/scripts/Log';
+import Points from './Points';
 
 export default function ({ app, ...props }) {
 	const { registry, systems } = app;
@@ -15,16 +16,7 @@ export default function ({ app, ...props }) {
 	delete props.sx;
 	return (
 		<Box sx={sx} {...props}>
-			<Stack direction="column" spacing={0}>
-				<LinearProgress variant="determinate" value={25} sx={{
-					height: progressHeight,
-				}}
-					color="success" />
-				<LinearProgress variant="determinate" value={25} sx={{
-					height: progressHeight,
-				}}
-					color="secondary" />
-			</Stack>
+			<Points app={app} />\
 		</Box>
 	);
 }
