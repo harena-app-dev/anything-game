a51db5ba4da9b4fb845985b1ebaec4e3bcf545ab
harena-app-dev <harena.app.dev@gmail.com>
2024-06-03 14:24:48 -0500

    w

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
-import { Alert, LinearProgress, Stack, TextField, Typography } from '@mui/material';
+import { Alert, LinearProgress, Stack, TextField, Typography, createTheme } from '@mui/material';
 import Log from '@/scripts/Log';
 import Chat from './Chat';
 import Status from './status/Status';
@@ -13,8 +13,6 @@ import { createRoot } from 'react-dom/client';
 
 export default function Scene({ app }) {
 	const [registry, systems] = app.get();
-
-	const client = systems.get('Client');
 	const raycaster = systems.get('Raycaster');
 	const moba = systems.get('Moba');
 	const onRightClick = (e) => {
@@ -24,39 +22,17 @@ export default function Scene({ app }) {
 		moba.moveToRpc(player.getPlayerEntity(), intersection.worldPosition);
 	}
 	useEffect(() => {
-		// Log.debug(`Scene.useEffect`);
 		const renderer = systems.get('Renderer')
-		// const uiJsx = <div className="" style={{ marginTop: "auto", marginLeft: "auto", marginRight: "auto" }}>
-		// Hii!
-		// </div>
 		renderer.setSceneElement(document.getElementById("scene"));
-		// const root = createRoot(renderer.getDomElement());
-		// root.render(uiJsx);
 		function loop() {
 			requestAnimationFrame(loop);
 			systems.tick();
 		}
 		loop();
-		const theme = app.theme;
-		theme.palette["health"] = {
-			main: 'green',
-		};
-		app.setTheme(theme);
 		return () => {
 			Log.debug(`Scene.useEffect return`);
 			renderer.onSceneElementResize();
 		};
-		// const intervalId = setInterval(() => {
-		// 	// console.log('This will run every second');
-		// 	systems.tick();
-		// 	// 	requestAnimationFrame(loop);
-		// }, 1000 / 100);
-
-		// Cleanup function
-		// return () => {
-		// 	clearInterval(intervalId);
-		// };
-
 	}, []);
 	const itemWidth = 32;
 
@@ -163,17 +139,6 @@ export default function Scene({ app }) {
 					</div>
 				</div>
 			</div>
-			{/* <div className='row' style={{ position: "absolute", width: itemWidth * 4, backgroundColor: 'rgba(0,0,0,0.5)' }}>
-				{
-					[...Array(8)].map((_, i) => (
-						<img src="textures/rogue.png"
-							key={i}
-							className='itemIcon' style={{ width: itemWidth, height: itemWidth }}
-						>
-						</img>
-					))
-				}
-			</div> */}
 		</Box >
 	);
 }
