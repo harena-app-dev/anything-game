d202f6e100c452d5fa833daa8fa3056b5133b084
harena-app-dev <harena.app.dev@gmail.com>
2024-06-03 09:59:40 -0500

    w

--- scripts/systems/Moba.js
+++ scripts/systems/Moba.js
@@ -6,25 +6,26 @@ export default function (registry, systems) {
 		if (client !== undefined) {
 			return;
 		}
-		registry.view('MoveGoal').each((goalEntity, moveGoal) => {
-			const goalPosition = registry.get('Position', goalEntity);
-			const objectPosition = registry.get('Position', moveGoal.entity);
+		registry.view('MoveGoal').each((entity, moveGoal) => {
+			const goalPosition = moveGoal.position;
+			const objectPosition = registry.get('Position', entity);
 			// let dx = moveGoal.position.x - position.x;
 			// let dy = moveGoal.position.y - position.y;
 			// let dz = moveGoal.position.z - position.z;
 			// const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
 			const distance = objectPosition.distanceTo(goalPosition);
-			Log.info(`Moba.tick`, { goalEntity, goalPosition, objectPosition, distance });
+			Log.info(`Moba.tick`, { goalEntity: entity, goalPosition, objectPosition, distance });
 			if (distance < 0.001) {
-				registry.erase('MoveGoal', goalEntity);
+				registry.erase('MoveGoal', entity);
 				return;
 			}
-			Log.info(`Moba.tick`, { goalEntity, goalPosition, moveGoal, distance });
+			// Log.info(`Moba.tick`, { goalEntity, goalPosition, moveGoal, distance });
 			const direction = goalPosition.clone().sub(objectPosition).normalize();
 			const speed = Math.min(distance, 0.1);
 			const delta = direction.clone().multiplyScalar(speed);
 			objectPosition.add(delta);
-			registry.replace('Position', moveGoal.entity, objectPosition);
+			// registry.replace('Position', moveGoal.entity, objectPosition);
+			registry.replace('Position', entity, objectPosition);
 
 		});
 	}
@@ -37,8 +38,8 @@ export default function (registry, systems) {
 		position.y = 0;
 		position.x = _position.x;
 		position.z = _position.z;
-		const goalEntity = registry.create();
-		const goal = registry.emplace('MoveGoal', goalEntity, { entity });
-		const goalPosition = registry.emplace('Position', goalEntity, position);
+		// const goalEntity = registry.create();
+		// const goal = registry.emplace('MoveGoal', goalEntity, { entity });
+		const goal = registry.emplaceOrReplace('MoveGoal', entity, { position });
 	}
 }
