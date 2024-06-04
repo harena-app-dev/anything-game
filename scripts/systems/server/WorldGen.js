10f2541228b64a1e87732b8c2d213e99ea2c8b15
harena-app-dev <harena.app.dev@gmail.com>
2024-06-04 08:54:25 -0500

    w

--- scripts/systems/server/WorldGen.js
+++ scripts/systems/server/WorldGen.js
@@ -4,23 +4,20 @@ export default function (registry) {
 	for (let x = -radius; x < radius; x++) {
 		for (let z = -radius; z < radius; z++) {
 			const entity = registry.create()
-			// registry.emplace("Position", entity, { x, y: -1, z })
 			registry.emplace("Position", entity, [x, -1, z])
-			// registry.emplace("Sprite", entity, { path: "grass1.png" })
 			registry.emplace("Block", entity, { texture: "grass1.png" })
 			const rng = Math.random()
 			if (rng < 0.1) {
 				const tree = registry.create()
 				const treeScale = 1.5;
-				// registry.emplace("Position", tree, { x, y: 0.25, z })
 				registry.emplace("Position", tree, [x, 0.25, z])
 				registry.emplace("Scale", tree, { x: treeScale, y: treeScale, z: treeScale })
 				registry.emplace("Sprite", tree, { path: "tree.png" })
 
-				const circleOutline = registry.create()
-				registry.emplace("Position", circleOutline, { x, y: 0.35, z })
-				registry.emplace("Scale", circleOutline, { x: treeScale, y: treeScale, z: treeScale })
-				registry.emplace("Sprite", circleOutline, { path: "whiteCircle.png" })
+				// const circleOutline = registry.create()
+				// registry.emplace("Position", circleOutline, { x, y: 0.35, z })
+				// registry.emplace("Scale", circleOutline, { x: treeScale, y: treeScale, z: treeScale })
+				// registry.emplace("Sprite", circleOutline, { path: "whiteCircle.png" })
 
 			} else if (rng < 0.15) {
 				const dirtWall = registry.create()
