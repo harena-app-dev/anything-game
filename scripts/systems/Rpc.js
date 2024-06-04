44de62d7cb6cdc4fe20d355dbd356deba0ad8aa1
harena-app-dev <harena.app.dev@gmail.com>
2024-06-04 09:14:48 -0500

    rpc try

--- scripts/systems/Rpc.js
+++ scripts/systems/Rpc.js
@@ -18,18 +18,21 @@ export default function (registry, systems) {
 			const parameterNames = getParameterNames(field);
 			rpcs[`${fieldName}Rpc`] = function (...args) {
 				const client = systems.get('Client');
-				// if a param name ends with 'entity', then map it to a server entity using client.e2s
 				const serverArgs = args.map((arg, i) => {
 					if (parameterNames[i].toLowerCase().endsWith('entity')) {
 						return client.c2s(arg);
 					}
 					return arg;
 				});
-				// systems.get('Wsm').getWsm().sendToAll(`${name}.${fieldName}`, ...args);
 				systems.get('Wsm').getWsm().sendToAll(`${name}.${fieldName}`, ...serverArgs);
+
 			}
 			wsm.addHandler(`${name}.${fieldName}`, (ws, ...args) => {
-				system[fieldName](...args);
+				try {
+					system[fieldName](...args);
+				} catch (error) {
+					Log.warn(`Error calling RPC ${name}.${fieldName}`, error);
+				}
 			})
 		}
 		for (let [rpcName, rpc] of Object.entries(rpcs)) {
